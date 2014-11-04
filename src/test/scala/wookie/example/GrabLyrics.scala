47687114208b704304ded750c85955c266ed3b8e
Andrey Chaschev <chaschev@gmail.com>
2014-11-04 22:55:18 +0700

    GrabLyrics - saves state to json and continues from previous stop

--- src/test/scala/wookie/example/GrabLyrics.scala
+++ src/test/scala/wookie/example/GrabLyrics.scala
@@ -1,8 +1,9 @@
 package wookie.example
 
-import java.io.{FileWriter, PrintWriter}
-import java.util.Date
+import java.io.File
+import java.net.URLEncoder
 
+import org.apache.commons.io.FileUtils
 import org.apache.commons.lang3.StringUtils
 import wookie._
 
@@ -12,28 +13,47 @@ import wookie._
 object GrabLyrics {
   val defaultPanel = SearchAndStarWookieSimple.defaultPanel
 
+  val FROM_LANGUAGE = "Spanish"
+  val TO_LANGUAGE = "English"
+
+  val HOST = "http://lyricstranslate.com"
+
+  val LYRICS_JSON = new File("lyrics.json")
+
   def newPanel: WookiePanel = defaultPanel.apply()
 
   def main(args: Array[String])
   {
     val app = WookieSandboxApp.start()
 
-    val writer = new PrintWriter(new FileWriter("lyrics.log", true))
-
-    writer.println(s"session started at ${new Date()}")
-
     app.runOnStage(
       new WookieSimpleScenario("Find Popular Foreign Lyrics!", defaultPanel) {
         override def run(): Unit = {
-          load("http://lyricstranslate.com/en/translations")
+          var grabState = if(!LYRICS_JSON.exists()) {
+            new GrabLyricsState()
+          } else {
+            JsonObjectUtils.fromJson[GrabLyricsState](FileUtils.readFileToString(LYRICS_JSON))
+          }
 
-          $("#edit-formtree-ltsearchlanguagefrom-hierarchical-select-selects-0").asSelect()
-            .findOptionByText("Spanish").get.select()
+          if(grabState.state == "finished") {
+            return
+          }
 
-          $("#edit-formtree-ltsearchlanguageto-hierarchical-select-selects-0").asSelect()
-            .findOptionByText("English").get.select()
+          val continueFromPage = grabState.lastUrl
 
-          $("#edit-formtree-submit").submit()
+          if(!continueFromPage.isDefined) {
+            load(s"$HOST/en/translations")
+
+            $("#edit-formtree-ltsearchlanguagefrom-hierarchical-select-selects-0").asSelect()
+              .findOptionByText(FROM_LANGUAGE).get.select()
+
+            $("#edit-formtree-ltsearchlanguageto-hierarchical-select-selects-0").asSelect()
+              .findOptionByText(TO_LANGUAGE).get.select()
+
+            $("#edit-formtree-submit").submit()
+          } else {
+            load(continueFromPage.get)
+          }
 
           var hasNextPage = true
 
@@ -49,25 +69,68 @@ object GrabLyrics {
                 val thanks = StringUtils.substringBetween(thanksText, "thanked ", " ").toInt
 
                 if(thanks >= 10) {
-                  val artistLink = row.find("td.ltsearch-translatenameoriginal")(0).find("a")(0).html()
-                  val songLink = row.find("td.ltsearch-translatenameoriginal")(1).find("a")(0).html()
+                  val artistLink = row.find("td.ltsearch-translatenameoriginal")(0).find("a")(0)
+                  val songLink = row.find("td.ltsearch-translatenameoriginal")(1).find("a")(0)
 
-                  writer.println(s"""$songLink ($thanks) by $artistLink""")
+                  val song = SongState(
+                    song = songLink.text().trim,
+                    songUrl = s"$HOST/${songLink.attr("href")}",
+                    artist = artistLink.text().trim,
+                    artistUrl = s"$HOST/${artistLink.attr("href")}",
+                    thanked = thanks
+                  )
+
+                  grabState = grabState.copy(
+                    songs =  grabState.songs :+ song
+                  )
                 }
               }
             }
-
-            writer.flush()
 
             val nextLink = $("#lyricstranslatesearch-searchtranslate-form ul.pager li").findResultWithText("next")
 
             hasNextPage = nextLink.isDefined
 
             if(hasNextPage) {
-              nextLink.get.find("a")(0).followLink()
+              val nextLinkA = nextLink.get.find("a")(0)
+              grabState = grabState.copy(lastUrl = Some(s"$HOST/${nextLinkA.attr("href")}"))
+              nextLinkA.followLink()
             }
+
+            FileUtils.writeStringToFile(LYRICS_JSON, JsonObjectUtils.toJson(grabState, pretty = true))
+
+            writeHtml(grabState)
           }
         }
       }.asNotSimpleScenario)
   }
+
+  def writeHtml(grabState: GrabLyricsState) = {
+    val htmlStr = grabState.songs.map { song =>
+      s"""
+        |(${song.thanked})
+        | <a href="${song.songUrl}">${song.song}</a>
+        | <a href="${song.artistUrl}">${song.artist}</a>
+        | <a href="http://www.youtube.com/results?search_query=${URLEncoder.encode(s"${song.artist} ${song.song}", "UTF-8")}">youtube</a>
+      """.stripMargin
+    }
+      .mkString("<br>\n")
+
+    FileUtils.writeStringToFile(new File("lyrics-2.html"), htmlStr)
+  }
 }
+
+case class GrabLyricsState(
+  lastUrl: Option[String] = None,
+  songs: List[SongState] = List(),
+  state: String = "running"
+)
+
+case class SongState(
+  song: String,
+  songUrl: String,
+  artist: String,
+  artistUrl: String,
+  thanked: Int
+)
+
---
+++ src/test/scala/wookie/example/JsonObjectUtils.scala
@@ -0,0 +1,64 @@
+package wookie.example
+
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+
+import org.json4s.{DefaultFormats, Formats}
+import org.json4s.jackson.JsonMethods._
+import org.json4s.jackson.Serialization
+
+import scala.util.control.NonFatal
+
+/**
+ * @author Andrey Chaschev chaschev@gmail.com
+ */
+object JsonObjectUtils {
+  implicit val formats: Formats =
+    new DefaultFormats {
+      override val dateFormatter = {
+        val formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"))
+        formatter
+      }
+    }
+
+  def toJson(obj: AnyRef, pretty:Boolean = false): String =
+  {
+    if(pretty) {
+      Serialization.writePretty(obj)
+    } else {
+      Serialization.write(obj)
+    }
+  }
+
+  /**
+   * Converts JSON to Scala object (case class, Map, Seq etc.).
+   * If you want to do more complicated things, you should use JSON4S directly:
+   * https://github.com/json4s/json4s
+   */
+  def fromJson[T](jsonString: String)(implicit m: Manifest[T]): T = {
+    // Serialization.read doesn't work without type hints.
+    //
+    // Serialization.read[Map[String, Any]]("""{"name": "X", "age": 45}""")
+    // will throw:
+    // org.json4s.package$MappingException: No information known about type
+    //
+    // JsonMethods.parse works for the above.
+    if (m.runtimeClass.getName.startsWith("scala")) {
+      try {
+        parse(jsonString, useBigDecimalForDouble = true).extract[T]
+      } catch {
+        case NonFatal(e) =>
+          Serialization.read[T](jsonString)
+      }
+    } else {
+      try {
+        Serialization.read[T](jsonString)
+      } catch {
+        case NonFatal(e) =>
+          val p = parse(jsonString, useBigDecimalForDouble = true)
+          p.extract
+      }
+    }
+  }
+}
